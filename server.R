7762a5283522eb8997a48985f5fb52231995f36d
FAHAD <eng.bokhari@hotmail.com>
2024-06-08 18:58:58 +1200

    Add files via upload
    Update on this var: 
    -  Create Main dashboard:  https://github.com/orgs/Heba-aletergi/projects/6/views/1?filterQuery=&pane=issue&itemId=66630460 
    -  Create General Statiscits page: https://github.com/orgs/Heba-aletergi/projects/6/views/1?filterQuery=&pane=issue&itemId=66630471

--- server.R
+++ server.R
@@ -1,3 +1,7 @@
+library(dplyr)
+library(ggplot2)
+library(plotly)
+
 #______________________ HELPER FUNCTION ________________________________________
 
 hist_x <- function(input) {
@@ -8,14 +12,105 @@ hist_x <- function(input) {
   return(ret)
 }
 
+# Main Dashboard -----------------------------
+main_dashboard <- function(output, data) {
+  
+  # Value box ---------------------------------------
+  count_petrol <- filter(data, MOTIVE_POWER == 'PETROL')
+  count_diesel <- filter(data, MOTIVE_POWER == 'DIESEL')
+  
+  output$box_val1 <- renderValueBox({ 
+    valueBox(formatC(nrow(data), format="d", big.mark=','),"Total Car Reg", icon = icon("stats",lib='glyphicon'),color = "green")  
+  })
+  output$box_val2 <- renderValueBox({ 
+    valueBox(
+      formatC(nrow(count_petrol), format="d", big.mark=','),"Total Petrol Car", icon = icon("line-chart"),color = "purple")  
+  })
+  output$box_val3 <- renderValueBox({
+    valueBox(
+      formatC(nrow(count_diesel), format="d", big.mark=','),"Total Diesel Car", icon = icon("menu-hamburger",lib='glyphicon'),color = "yellow")  
+  })
+  
+  # Top 5 Brand Plot ----------------------------------
+  make_freq <- sort(table(data$MAKE), decreasing = TRUE)
+  make_top <- head(make_freq,5)
+  make_freq_df <- as.data.frame(make_top)
+  colnames(make_freq_df) <- c("MAKE", "Count")
+  
+  output$main_plot1 <- renderPlotly({
+    plot_ly(make_freq_df, x = ~MAKE, y = ~Count, type = 'bar', name = '') %>%
+      layout(title = "Bar Chart",
+           xaxis = list(title = "Brand"),
+           yaxis = list(title = "Brand Count"))       
+  })
+  
+  
+  # Distribution of body Type pie chart ---------------------
+  body_type <- head(table(data$BODY_TYPE))
+  body_type_df <- as.data.frame(body_type)
+  colnames(body_type_df) <- c("Character", "Frequency")
+  
+  output$pieChart <- renderPlotly({
+    plot_ly(body_type_df, labels = ~Character, values = ~Frequency, type = 'pie') %>%
+      layout(title = "")
+  })
+  
+  y_ <- "IMPORT_STATUS"
+ 
+  output$MD_top5_txt <- renderText({
+    paste("You chose", c)
+  })
+}
+
+# General Statistics Page --------------------
+general_stat <- function(output, data) {
+  
+  # Calculate min and max values for each column
+  summary_stats <- apply(data[, -1], 2, function(x) c(min = min(x), max = max(x)))
+  
+  output$DataTable <- DT::renderDataTable({
+    DT::datatable(data[,c("MAKE","BODY_TYPE","ORIGINAL_COUNTRY","TLA","MOTIVE_POWER","IMPORT_STATUS","VEHICLE_YEAR")], filter = "top", 
+          options = list(pageLength = 10)
+      )
+  })
+}
+
+#______________________ SERVER FUNCTION ________________________________________
+
 server <- function(input, output, session){
   # Read data from .CSV
+  data <- read.csv("VehicleYear-2023.csv")
+  make <- data[0:2,c("MAKE")]
+  
+  # MAin Dashboard
+  main_dashboard(output, data)
+  
+  # General Statistics Page
+  general_stat(output,data)
   
   
-  # Call when chaged Select Input (select_make)
+  # Fill SelectInput (select_make) with dataset value
+  observe({
+    updateSelectInput(session, "select_make",
+                      choices = unique(data$MAKE),
+                      selected = "Select Vehicle")
+  })
+  
+  # Reactive with widget (textOutput)  
+  output$R_btn_val <- renderText({
+    paste("You chose", input$radio_feat_1)
+  })
+  
+  # Reactive with widget ()
+  output$radioGroup  <- renderUI({
+    radioButtons("radio_feat_1", "Select an option", 
+                 choices = unique(data$MAKE),
+                 selected = "opt2")
+  })
+  
+  # Call when SelectInput(select_make) chaged 
   observeEvent(input$select_make, {
-    # Read select Input value 
-    selected <- as.numeric(input$select_make)
+    selected <- as.numeric(input$select_make)  # Read select Input value and convert it to numeric to use it for histogram
     output$distPlot <- renderPlot({
       hist_param <- hist_x(input)
       hist(hist_param$x, breaks = selected, col = "#007bc2", border = "white",
@@ -23,7 +118,7 @@ server <- function(input, output, session){
            main = "Histogram of waiting times")
     })
   })
-  # Call when change sliderInput (bins)
+  # Call when sliderInput(bins) chaged.
   observeEvent(input$bins, {
     output$distPlot <- renderPlot({
       hist_param <- hist_x(input)
--- ui.R
+++ ui.R
@@ -1,21 +1,28 @@
+library(shiny)
+library(shinydashboard)
+library(bslib)
+library(plotly)
+
+
 # USER INTERFACE FUCNTION 
 
 ui <- function(request) {
-  sidebar <- dashboardSidebar(
-    hr(),
+#----------------------------- MUNU BAR SIDE -----------------------------------
+  sidebar <- dashboardSidebar(disable = FALSE,
     sidebarMenu(id="tabs",
-                menuItem("Main Dashboard", tabName="plot", icon=icon("line-chart")),
-                menuItem("General Statistics", tabName = "global", icon=icon("table"), selected=TRUE),
-                menuItem("Single Vehicle Features",
-                         menuSubItem("Model VS Vehicle Year", tabName = "s_model_year", icon = icon("angle-right")),
-                         menuSubItem("global.R", tabName = "hhh", icon = icon("angle-right"))
-                ),
-                menuItem("Two Vehicles",
-                         menuSubItem("global.R", tabName = "tt", icon = icon("angle-right")),
-                         menuSubItem("ui.R", tabName = "ui", icon = icon("angle-right")),
-                         menuSubItem("server.R", tabName = "server", icon = icon("angle-right")),
-                         menuSubItem("structure", tabName = "structure", icon = icon("angle-right"))
-                )
+            #menuItem("Main Dashboard", tabName="plot", icon=icon("line-chart")),
+            menuItem("Main Dashboard", tabName = "main", icon=icon("line-chart"), selected=TRUE),
+            menuItem("General Statistics", tabName = "data_table", icon=icon("table")),
+            menuItem("Single Vehicle Features",
+                     menuSubItem("Model VS Vehicle Year", tabName = "s_model_year", icon = icon("angle-right")),
+                     menuSubItem("global.R", tabName = "hhh", icon = icon("angle-right"))
+            ),
+            menuItem("Two Vehicles",
+                     menuSubItem("global.R", tabName = "tt", icon = icon("angle-right")),
+                     menuSubItem("ui.R", tabName = "ui", icon = icon("angle-right")),
+                     menuSubItem("server.R", tabName = "server", icon = icon("angle-right")),
+                     menuSubItem("structure", tabName = "structure", icon = icon("angle-right"))
+            )
     ),
     hr(),
     conditionalPanel("input.tabs == 'plot'",
@@ -23,13 +30,43 @@ ui <- function(request) {
                      )
     )
   )
-  # 
-  body <- dashboardBody(
+#----------------------------- MAIN CONTENT ------------------------------------
+body <- dashboardBody(
     tabItems(
-      # Page ( General Statistics) -------------------------------------------
-      tabItem(tabName = "global",
-              box(width = NULL, status = "primary", solidHeader = FALSE, title="Table",
-                  
+      # Page (General Statistics)
+      tabItem(tabName = "data_table",
+              fluidRow(
+                box(width = 12),
+                box(status = "primary", background = NULL, width = 12,
+                    DT::dataTableOutput("DataTable"))
+                ),
+              fluidRow(
+                
+              )
+      ),
+      # Page (Main Dashboard) ---------------------------------------
+      tabItem(tabName = "main",
+              fluidRow(
+                valueBoxOutput("box_val1"),
+                valueBoxOutput("box_val2"),
+                valueBoxOutput("box_val3")
+              ),
+              fluidRow(
+                box(
+                  title = "Top 5 brand Reg"
+                  ,status = "primary"
+                  ,solidHeader = FALSE 
+                  ,collapsible = TRUE 
+                  ,plotlyOutput("main_plot1", height = "300px")
+                  #textOutput("MD_top5_txt")
+                ),
+                box(
+                  title = "Data Distribution by Body Type"
+                  ,status = "primary"
+                  ,solidHeader = FALSE 
+                  ,collapsible = TRUE 
+                  ,plotlyOutput("pieChart", height = "300px")
+                )
               )
       ),
       # Page (Single Vehicle --> Model vs VehicleYear) -----------------------
@@ -37,18 +74,25 @@ ui <- function(request) {
               # Filter Panel ------------------------------
               card(
                 layout_columns(
-                  card(
-                    selectInput("select_make", label = h3("Select Vehicle Brand"), 
+                  box(status = "primary", width = 11,
+                    selectInput("select_make", label = h4("SELECT VEHICLE BRAND"), 
                                 choices = list("5" = 5, "10" = 10, "15" = 15), 
                                 selected = 1,selectize = FALSE),
                     
                   ),
-                  card(
-                    sliderInput(inputId = "bins", label = "Number of bins:",
-                                min = 1, max = 50, value = 30)
+                  box(status = "primary", width = 11,
+                    #"VEHICLE_YEAR"  ,  "TLA" , "ORIGINAL_COUNTRY" , "MOTIVE_POWER" , "IMPORT_STATUS" , "FIRST_NZ_REGISTRATION_YEAR" , 
+                    radioButtons("radio_feat_1", label = h4("SELECT FEATURE 1"),
+                                 choices = list("MOTIVE_POWER", "IMPORT_STATUS", "FIRST_NZ_REGISTRATION_YEAR"),
+                                 selected = 1)
                   ),
-                  card(
-                    verbatimTextOutput("select_make")  # Display selected option for(select_make)
+                  box(status = "primary", width = 11,
+                    radioButtons("radio_feat_2", label = h4("SELECT FEATURE 2"),
+                                 choices = list("VEHICLE_YEAR", "TLA", "ORIGINAL_COUNTRY"),
+                                 selected = 1),
+                    sliderInput(inputId = "bins", label = "Number of bins:",
+                                min = 1, max = 50, value = 30),
+                    textOutput("R_btn_val")
                   )
                 ),
                 br(),
